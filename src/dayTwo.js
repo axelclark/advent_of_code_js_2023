c384ad3aae424e7e7627b4b83657b7b80e38b0a2
Axel Clark <axel.clark@pm.me>
2024-01-21 19:42:40 -0800

    Refactor Day Two

--- src/dayTwo.js
+++ src/dayTwo.js
@@ -1,34 +1,36 @@
-const partOne = (input) => {
+const partOne = function partOne(input) {
   const games = input.map(parseLine);
   const possibleGames = games.filter(isPossible);
   return possibleGames.reduce((acc, { game }) => acc + game, 0);
 };
 
-const parseLine = (line) => {
+const setMaxResults = function setMaxResults(maxResults, handfull) {
+  [...handfull].forEach(([color, value]) => {
+    if (maxResults.has(color)) {
+      maxResults.set(color, Math.max(maxResults.get(color), value));
+    } else {
+      maxResults.set(color, value);
+    }
+  });
+
+  return maxResults;
+};
+
+const parseLine = function parseLine(line) {
   const [rawGame, rawResults] = line.replace("Game ", "").split(":");
   const game = Number.parseInt(rawGame);
   const parsedResults = rawResults.split(";").map(parseHandfull);
-  const maxResults = new Map();
-  const results = parsedResults.reduce((acc, handfull) => {
-    for (const [color, value] of handfull) {
-      if (acc.has(color)) {
-        acc.set(color, Math.max(acc.get(color), value));
-      } else {
-        acc.set(color, value);
-      }
-    }
-    return acc;
-  }, maxResults);
+  const results = parsedResults.reduce(setMaxResults, new Map());
   return { game, results };
 };
 
-const parseHandfull = (handfull) => {
+const parseHandfull = function parseHandfull(handfull) {
   const trimmedHandfull = handfull.trim();
   const results = trimmedHandfull.split(",").map(parseCubeResult);
   return new Map(results);
 };
 
-const parseCubeResult = (cubeResult) => {
+const parseCubeResult = function parseCubeResult(cubeResult) {
   const trimmedCubeResult = cubeResult.trim();
   const [number, color] = trimmedCubeResult.split(" ").map((x) => x.trim());
   return [color, Number.parseInt(number)];
@@ -40,7 +42,7 @@ const constraints = new Map([
   ["blue", 14],
 ]);
 
-const isPossible = ({ results }) => {
+const isPossible = function isPossible({ results }) {
   const gameIsPossible = Array.from(results).every(([color, value]) => {
     const maxValue = constraints.get(color);
     const isPossibleForColor = maxValue >= value;
@@ -49,16 +51,19 @@ const isPossible = ({ results }) => {
   return gameIsPossible;
 };
 
-const calculate_power = ({ results }) => {
-  return Array.from(results).reduce((acc, [_color, value]) => {
+const calculatePower = function calculatePower({ results }) {
+  const cubeValues = results.values();
+  const power = [...cubeValues].reduce((acc, value) => {
     return acc * value;
   }, 1);
+
+  return power;
 };
 
-const partTwo = (input) => {
+const partTwo = function partTwo(input) {
   const result = input
     .map(parseLine)
-    .map(calculate_power)
+    .map(calculatePower)
     .reduce((acc, power) => acc + power, 0);
   return result;
 };
